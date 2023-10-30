12ab6e1085bf5c5680cc76a6586c16d6635c3ff2
Atikur Rahman <mratikurrahman@gmail.com>
2023-10-30 16:56:09 +0600

    CreateView, HistoryView and SettingsView have created

---
+++ QRAndBarCodeScanner/Screens/CreateView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateView.swift
+//  QRAndBarCodeScanner
+//
+//  Created by Atikur Rahman on 10/30/23.
+//
+
+import SwiftUI
+
+struct CreateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateView()
+}
