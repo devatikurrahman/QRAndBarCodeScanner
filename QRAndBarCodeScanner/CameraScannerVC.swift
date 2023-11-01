5d0dcc5f2da9d354c56597beca6da62054be74ad
Atikur Rahman <mratikurrahman@gmail.com>
2023-11-01 23:55:42 +0600

    Updated code

--- QRAndBarCodeScanner/CameraScannerVC.swift
+++ QRAndBarCodeScanner/CameraScannerVC.swift
@@ -14,7 +14,7 @@ protocol CameraScannerVCDelegate: AnyObject {
 
 final class CameraScannerVC: UIViewController {
     let captureSession = AVCaptureSession()
-    var previewLayer: AVCaptureVideoPreviewLayer
+    var previewLayer: AVCaptureVideoPreviewLayer?
     weak var scannerDelegate: CameraScannerVCDelegate?
     
     init(scannerDelegate: CameraScannerVCDelegate) {
@@ -25,5 +25,47 @@ final class CameraScannerVC: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func setupCaptureSession() {
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+            return
+        }
+        
+        let videoInput: AVCaptureDeviceInput
+        do {
+            try videoInput = AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+        
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        } else {
+            return
+        }
+        
+        let metaDataOutput = AVCaptureMetadataOutput()
+        if captureSession.canAddOutput(metaDataOutput) {
+            captureSession.addOutput(metaDataOutput)
+            metaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metaDataOutput.metadataObjectTypes = [.ean8, .ean13, .qr]
+        } else {
+            return
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer?.videoGravity = .resizeAspectFill
+        if let previewLayer = previewLayer {
+            view.layer.addSublayer(previewLayer)
+        } else {
+            return
+        }
+        
+        captureSession.startRunning()
+    }
+}
+
+extension CameraScannerVC: AVCaptureMetadataOutputObjectsDelegate {
+    
 }
 
